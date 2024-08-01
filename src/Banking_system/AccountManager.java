aca4926026d0624e7d3372b8cd018762e27c1704
Deep <gaonerdeep@gmail.com>
2024-08-01 11:45:57 +0530

    first commit

--- src/Banking_system/AccountManager.java
+++ src/Banking_system/AccountManager.java
@@ -6,16 +6,25 @@ public class AccountManager {
     Scanner scan = new Scanner(System.in);
     public void credit_money(long account_number)
     {
-        System.out.println("Enter amount ");
+        System.out.print("Enter amount ");
         double amount = scan.nextLong();
-        System.out.println("enter security pin");
+        System.out.print("enter security pin ");
         String security_pin = scan.next();
     }
     public void debit_money(long account_number)
     {
-        System.out.println("enter amount ");
+        System.out.print("enter amount ");
         double amount = scan.nextLong();
-        System.out.println("enter security pin");
+        System.out.print("enter security pin ");
+        String security_pin = scan.next();
+    }
+    public void transfer_money(long sender_account_number)
+    {
+        System.out.print("enter receiver account number ");
+        long receiver_account_number = scan.nextLong();
+        System.out.print("enter amount ");
+        double amount = scan.nextDouble();
+        System.out.print("enter security pin ");
         String security_pin = scan.next();
     }
 }
--- src/Banking_system/BankingApp.java
+++ src/Banking_system/BankingApp.java
@@ -5,5 +5,6 @@ import java.util.Scanner;
 
 public class BankingApp {
     public static void main(String[] args) {
+
     }
 }
